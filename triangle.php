f7811e6d77ba8a39e560305f2b12abd114379e5b
Jordan Meier and Ryan Brown <browneryan87@gmail.com>
2016-02-08 14:40:15 -0800

    Add basic php structure

---
+++ triangle.php
@@ -0,0 +1,30 @@
+<?php
+    class Triangle
+    {
+        private $side1;
+        private $side2;
+        private $side3;
+    }
+
+    function getSide1()
+    {
+        return $side1;
+    }
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Triangles</title>
+</head>
+<body>
+    <h1>Is Triangle?</h1>
+    <ul>
+        <?php
+        ?>
+    </ul>
+</body>
+</html>
