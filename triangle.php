8161b5b8de32d28658a885102fe81298db9ca5f4
Jordan Meier and Ryan Brown <browneryan87@gmail.com>
2016-02-08 14:46:56 -0800

    add getters for all private properties

--- triangle.php
+++ triangle.php
@@ -4,11 +4,29 @@
         private $side1;
         private $side2;
         private $side3;
+
+        function __construct($parcel_weight, $parcel_height, $parcel_width, $parcel_length)
+        {
+            $this->side1 = $side_one;
+            $this->side2 = $side_two;
+            $this->side3 = $side_three;
+
+        }
     }
 
     function getSide1()
     {
-        return $side1;
+        return $this->side1;
+    }
+
+    function getSide2()
+    {
+        return $this->side2;
+    }
+
+    function getSide3()
+    {
+        return $this->side3;
     }
 
 
@@ -24,6 +42,7 @@
     <h1>Is Triangle?</h1>
     <ul>
         <?php
+            $new_triangle = new Triangle ($side_one, $side_two, $side_three);
         ?>
     </ul>
 </body>
